17154ed8c79f6061032214de976d884dc03fe17a
Cantte <cantte098@gmail.com>
2020-09-20 18:36:57 -0500

    feat: Update MatFileInputModule and public-api
    In-Module: Add FocusMonitor provider and export ByteFormatPipe
    In-public-api: Export MatFileInputModule, MatFileInputComponent,
    FileInput, FileInputConfig, ByteFormatPipe and maxContentSize

--- projects/mat-file-input/src/lib/mat-file-input.module.ts
+++ projects/mat-file-input/src/lib/mat-file-input.module.ts
@@ -1,10 +1,11 @@
+import { FocusMonitor } from '@angular/cdk/a11y';
 import { NgModule } from '@angular/core';
 import { MatFileInputComponent } from './mat-file-input/mat-file-input.component';
 import { ByteFormatPipe } from './pipes/byte-format.pipe';
 
 @NgModule({
   declarations: [ MatFileInputComponent, ByteFormatPipe ],
-  imports: [],
-  exports: [ MatFileInputComponent ]
+  providers: [ FocusMonitor ],
+  exports: [ MatFileInputComponent, ByteFormatPipe ]
 })
 export class MatFileInputModule {}
