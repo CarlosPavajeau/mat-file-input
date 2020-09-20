d36b95d5aefa61236f5b8dcf944fa16fcbab1865
Cantte <cantte098@gmail.com>
2020-09-20 16:59:19 -0500

    fix: Fix build

--- projects/mat-file-input/src/public-api.ts
+++ projects/mat-file-input/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of mat-file-input
  */
 
-export * from './lib/mat-file-input.service';
-export * from './lib/mat-file-input.component';
 export * from './lib/mat-file-input.module';
+export * from './lib/mat-file-input/mat-file-input.component';
