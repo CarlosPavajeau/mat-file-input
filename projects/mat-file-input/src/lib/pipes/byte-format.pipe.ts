876e5a42a791f458ff51ce82ab5eec2a0c949233
Cantte <cantte098@gmail.com>
2020-09-20 16:06:32 -0500

    feat: Add byte-format pipe

---
+++ projects/mat-file-input/src/lib/pipes/byte-format.pipe.ts
@@ -0,0 +1,49 @@
+import { Inject, Optional, Pipe, PipeTransform } from '@angular/core';
+import { FileInputConfig, MAT_FILE_INPUT_CONFIG } from '../model/file-input-config';
+
+@Pipe({
+  name: 'byteFormat'
+})
+export class ByteFormatPipe implements PipeTransform {
+  private unit: string;
+
+  constructor(
+    @Optional()
+    @Inject(MAT_FILE_INPUT_CONFIG)
+    private config: FileInputConfig
+  ) {
+    this.unit = this.config ? this.config.sizeUnit : 'Byte';
+  }
+
+  transform(value: any, args?: any): any {
+    if (parseInt(value, 10) >= 0) {
+      value = this.formatBytes(+value, +args);
+    }
+    return value;
+  }
+
+  private formatBytes(bytes: number, decimals?: number): string {
+    if (bytes === 0) {
+      return '0' + this.unit;
+    }
+
+    const unitChar = this.unit.charAt(0);
+    const kilobytes = 1024;
+    const decimalsCount = decimals || 2;
+    const sizes = [
+      this.unit,
+      'K' + unitChar,
+      'M' + unitChar,
+      'G' + unitChar,
+      'T' + unitChar,
+      'P' + unitChar,
+      'E' + unitChar,
+      'Z' + unitChar,
+      'Y' + unitChar
+    ];
+
+    const byteScale = Math.floor(Math.log(bytes) / Math.log(kilobytes));
+
+    return parseFloat((bytes / Math.pow(kilobytes, byteScale)).toFixed(decimals)) + ' ' + sizes[byteScale];
+  }
+}
