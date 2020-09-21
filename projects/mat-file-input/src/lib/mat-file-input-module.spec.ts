fcce073123c46dd9d6831628943a159ffcbf49ee
Cantte <cantte098@gmail.com>
2020-09-20 19:25:18 -0500

    test: Add all component tests

---
+++ projects/mat-file-input/src/lib/mat-file-input-module.spec.ts
@@ -0,0 +1,7 @@
+import { MatFileInputModule } from './mat-file-input.module';
+
+describe('MatFileInputModule', () => {
+  it('should work', () => {
+    expect(new MatFileInputModule()).toBeDefined();
+  });
+});
--- projects/mat-file-input/src/lib/pipes/byte-format.pipe.spec.ts
+++ projects/mat-file-input/src/lib/pipes/byte-format.pipe.spec.ts
@@ -1,8 +1,48 @@
+import { TestBed } from '@angular/core/testing';
+import { FileInputConfig, MAT_FILE_INPUT_CONFIG } from './../model/file-input-config';
 import { ByteFormatPipe } from './byte-format.pipe';
 
 describe('ByteFormatPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ByteFormatPipe();
-    expect(pipe).toBeTruthy();
+  let pipe: ByteFormatPipe;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ ByteFormatPipe ]
+    });
+    pipe = TestBed.inject(ByteFormatPipe);
+  });
+
+  it('should format a given value', () => {
+    const text = pipe.transform(104857600);
+    expect(text).toBe('100 MB');
+  });
+
+  it('should not format 0 value', () => {
+    const text = pipe.transform(0);
+    expect(text).toBe('0 Byte');
+  });
+});
+
+describe('ByteFormatPipe with injection token', () => {
+  let pipe: ByteFormatPipe;
+  const config: FileInputConfig = {
+    sizeUnit: 'Octet'
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ ByteFormatPipe, { provide: MAT_FILE_INPUT_CONFIG, useValue: config } ]
+    });
+    pipe = TestBed.inject(ByteFormatPipe);
+  });
+
+  it('should format a gicen value', () => {
+    const text = pipe.transform(104857600);
+    expect(text).toBe('100 MO');
+  });
+
+  it('should format 0 value', () => {
+    const text = pipe.transform(0);
+    expect(text).toBe('0 Octet');
   });
 });
