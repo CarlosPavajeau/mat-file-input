454a29270d98589a1d14282858f789a7202932b3
Cantte <cantte098@gmail.com>
2020-09-21 18:10:08 -0500

    fix: Fix module import for other packages

--- projects/mat-file-input/src/public-api.ts
+++ projects/mat-file-input/src/public-api.ts
@@ -2,9 +2,9 @@
  * Public API Surface of mat-file-input
  */
 
-export { MatFileInputModule } from './lib/mat-file-input.module';
-export { MatFileInputComponent } from './lib/mat-file-input/mat-file-input.component';
-export { FileInput } from './lib/model/file-input';
-export { FileInputConfig, MAT_FILE_INPUT_CONFIG } from './lib/model/file-input-config';
-export { ByteFormatPipe } from './lib/pipes/byte-format.pipe';
-export { maxContentSize } from './lib/validator/file-validator';
+export * from './lib/mat-file-input.module';
+export * from './lib/mat-file-input/mat-file-input.component';
+export * from './lib/model/file-input';
+export * from './lib/model/file-input-config';
+export * from './lib/pipes/byte-format.pipe';
+export * from './lib/validator/file-validator';
